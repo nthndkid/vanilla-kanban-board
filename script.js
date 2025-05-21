fbccfe61fddbdc43ff140411261d11ec01eb54ef
nthndkid <143003322+nthndkid@users.noreply.github.com>
2025-05-22 00:36:55 +0800

    added functionality

---
+++ script.js
@@ -0,0 +1,47 @@
+const cards = document.querySelectorAll(".card");
+const lists = document.querySelectorAll(".list");
+
+for(const card of cards){
+    card.addEventListener("dragstart", dragStart)
+    card.addEventListener("dragend", dragEnd)
+}
+
+for(const list of lists){
+    list.addEventListener("dragover", dragOver)
+    list.addEventListener("dragenter", dragEnter)
+    list.addEventListener("dragleave", dragLeave)
+    list.addEventListener("drop", dragDrop)
+}
+
+// allows to drop location to know which element is being moved when release on drag
+function dragStart(e) {
+    e.dataTransfer.setData("text/plain", this.id);
+}
+
+function dragEnd(){
+    console.log("Drag ended");
+}
+
+
+function dragOver(e){
+    e.preventDefault(); // override browser's default drop behavior
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+
+    this.classList.add("over");
+}
+
+function dragLeave(e){
+    this.classList.remove("over");
+}
+
+function dragDrop(e){
+    const id = e.dataTransfer.getData("text/plain");
+
+    const card = document.getElementById(id);
+
+    this.appendChild(card);
+    this.classList.remove("over");
+}
